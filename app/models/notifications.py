08458d63c2a1b12a4f9ff92745bf09528cec4d79
Jerry <jerryoommen07@gmail.com>
2026-02-05 22:32:09 -0500

    add Notifications model with attributes for user notifications management

---
+++ app/models/notifications.py
@@ -0,0 +1,24 @@
+import enum
+import uuid
+
+from sqlalchemy import Column, String, Enum, Boolean, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+
+from app.db.base import Base
+
+
+class NotificationsType(str, enum.Enum):
+    order_update = "order_update"
+    message = "message"
+    stock_alert = "stock_alert"
+
+
+class Notifications(Base):
+    __tablename__ = "notifications"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4)
+    type_ = Column(Enum(NotificationsType), nullable=False, default=NotificationsType.order_update)
+    message = Column(String(128), nullable=False)
+    is_read = Column(Boolean, nullable=False, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
