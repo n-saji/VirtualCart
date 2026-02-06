57d60ae07edb093740496f15b004977a747620b6
Jerry <jerryoommen07@gmail.com>
2026-02-05 21:45:58 -0500

    add CartItems model with attributes for cart management

---
+++ app/models/carts_items.py
@@ -0,0 +1,16 @@
+import uuid
+
+from sqlalchemy import Column, Integer, Float
+from sqlalchemy.dialects.postgresql import UUID
+
+from app.db.base import Base
+
+
+class CartItems(Base):
+    __tablename__ = "cart_items"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
+    cart_id = Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4)
+    product_id = Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4)
+    quantity = Column(Integer, nullable=False, default=0)
+    price_at_time = Column(Float, nullable=False, default=0)
